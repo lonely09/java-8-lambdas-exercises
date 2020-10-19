ad85c9d385f174c4ce37e1a18ef3bf2cde2c6172
lonely09 <53104577+lonely09@users.noreply.github.com>
2020-10-19 10:05:24 +0800

    Update Question2Test.java

--- src/test/java/com/insightfullogic/java8/exercises/chapter2/Question2Test.java
+++ src/test/java/com/insightfullogic/java8/exercises/chapter2/Question2Test.java
@@ -16,6 +16,7 @@ public class Question2Test {
         cal.set(Calendar.DAY_OF_MONTH, 1);
         String formatted = Question2.formatter.get().getFormat().format(cal.getTime());
         assertEquals("01-Jan-1970", formatted);
+        //获取时间
     }
 
 }
